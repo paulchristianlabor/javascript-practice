048ad8d81f091f28c5a81f9bde100782665e2c05
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2023-06-20 15:37:15 +0800

    adding elements

--- form-javascript/script.js
+++
@@ -1,8 +0,0 @@
-function submit(){
-    a = document.getElementById("firstName").value
-    b = document.getElementById("lastName").value
-    c = document.getElementById("email").value
-    d = document.getElementById("age").value
-    e = {firstName: a, lastName: b, email: c, age: d,}
-    alert(JSON.stringify(e))
-}
